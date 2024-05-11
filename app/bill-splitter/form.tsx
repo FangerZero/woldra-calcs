6e0f809f86d1d4f4ccd3c5cd09efaaa9450f18d3
FangerZero <FangerZero@hotmail.com>
2024-05-11 00:36:41 -0500

    fix: formatting and decimal issue

--- app/bill-splitter/form.tsx
+++ app/bill-splitter/form.tsx
@@ -12,7 +12,7 @@ import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 export default function Form() {
   const blankTennent = {name: "", startDate: new Date(), endDate: new Date(), bill: 0, dayPercent: 0};
   const [tennentList, setTennentList] = useState([blankTennent]);
-  const [bill, setBill] = useState({amount: 0.00, startDate: new Date(), endDate: new Date()});
+  const [bill, setBill] = useState({amount: "0.00", startDate: new Date(), endDate: new Date()});
 
   const splitBill = () => {
     const billDays = dateDeltaAddOne(bill.startDate, bill.endDate);
@@ -21,9 +21,9 @@ export default function Form() {
     let finalTennentList = newTennentList.map(tennent => {return {...tennent, bill: getTennentPayment(tennent.dayPercent, sumOfTennentsPercent, bill.amount)}});
     const billTotal = roundToHundredth(finalTennentList.reduce((accumulator, tennent) => accumulator + tennent.bill, 0));
 
-    if (billTotal !== bill.amount) {
+    if (billTotal !== +bill.amount) {
       let sortedTennentList = finalTennentList.toSorted(compareDesc);
-      let leftOverPennies = roundToHundredth(bill.amount - billTotal) * 100;
+      let leftOverPennies = roundToHundredth(+bill.amount - billTotal) * 100;
       const iterator = sortedTennentList.entries();
       let currentTennent = iterator.next().value;
       while(currentTennent !== undefined) {
@@ -49,6 +49,12 @@ export default function Form() {
     //  Tennent Pay = Tennent % * (billAmount / (Sum of Tennent %))
     return roundToHundredth(tenPercent * (billAmount / sumOfTennentsPercent));
   }
+
+  const formatPayment = (payment: number) => {
+    return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(
+      payment,
+    );
+  };
 
 
   //============================
@@ -121,7 +127,7 @@ export default function Form() {
   return (
     <div>
         <div className="pb-4">      
-            <TextField className="my-2 lg:my-0"  id="bill-amt" label="Utility Amount" value={bill.amount} variant="outlined" onChange={e => {setBill({...bill, amount: +e.target.value})}}/>
+            <TextField className="my-2 lg:my-0"  id="bill-amt" label="Utility Amount" value={bill.amount} variant="outlined" onChange={e => {setBill({...bill, amount: e.target.value})}}/>
             <LocalizationProvider dateAdapter={AdapterDayjs}>
                 <DatePicker className="my-2 lg:my-0 lg:px-1" label="Start Date" defaultValue={dayjs(new Date())} value={dayjs(bill.startDate)} onChange={newVal => setBill({...bill, startDate: newVal ? newVal.toDate() : new Date()})} />
                 <DatePicker className="my-2 lg:my-0 lg:px-1" label="End Date" defaultValue={dayjs(new Date())} value={dayjs(bill.endDate)} onChange={newVal => setBill({...bill, endDate: newVal ? newVal.toDate() : new Date()})} />
@@ -135,7 +141,7 @@ export default function Form() {
               <DatePicker className="my-2 lg:my-0 lg:px-1" label="Start Date" defaultValue={dayjs(new Date())} value={dayjs(tennent.startDate)} onChange={newVal => updateTennent(index, newVal, "startDate")} />
               <DatePicker className="my-2 lg:my-0 lg:px-1" label="End Date" defaultValue={dayjs(new Date())} value={dayjs(tennent.endDate)} onChange={newVal => updateTennent(index, newVal, "endDate")} />
             </LocalizationProvider>
-            <TextField disabled id="tennent-bill" className="my-2 lg:my-0 lg:px-1" label="Bill Amount" value={tennent.bill} variant="outlined" />
+            <TextField disabled id="tennent-bill" className="my-2 lg:my-0 lg:px-1" label="Bill Amount" value={formatPayment(tennent.bill)} variant="outlined" />
             {tennentList.length > 1 && <RemoveCircleOutline className="my-2 lg:my-0" onClick={() => removeTennent(index)} />}
             {index === (tennentList.length - 1) && <AddCircleOutline className="my-2 lg:my-0" onClick={addTennent} />}
         </div>
