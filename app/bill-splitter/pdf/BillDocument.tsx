75c77d8919816892f03766ad7a1eabbd0256275c
FangerZero <FangerZero@hotmail.com>
2025-04-27 00:53:16 -0500

    fix: format currency in pdf

--- app/bill-splitter/pdf/BillDocument.tsx
+++ app/bill-splitter/pdf/BillDocument.tsx
@@ -4,10 +4,19 @@ import { Document, Page, View, Text, StyleSheet } from '@react-pdf/renderer';
 const styles = StyleSheet.create({
     page: { backgroundColor: 'white' },
     billSection: { color: 'black', textAlign: 'center', margin: 20 },
-    tennentSection: { color: 'white', textAlign: 'left', marginHorizontal: 100 },
-    evenLine: {backgroundColor: 'gray', paddingHorizontal: 25, paddingVertical: 5},
-    oddLine: {color: 'black', backgroundColor: 'lightgray', paddingHorizontal: 25, paddingVertical: 5}
+    tennentSection: { color: 'white', textAlign: 'left', marginHorizontal: 100, },
+    evenLine: {backgroundColor: 'gray', paddingHorizontal: 25, paddingVertical: 5, flexDirection: 'row'},
+    oddLine: {color: 'black', backgroundColor: 'lightgray', paddingHorizontal: 25, paddingVertical: 5, flexDirection: 'row'},
+    tennentName: {fontWeight: 'bold', textAlign: 'left'},
+    tennentDates: {paddingHorizontal: 10, width: '75%'},
+    tennentBill: {paddingHorizontal: 10, textAlign: 'right'}
   });
+
+const formatPayment = (payment: number) => {
+    return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(
+      payment,
+    );
+  };
 
 const BillDocument = ({data}: any) => {
   const {bill, tennentList} = data;
@@ -16,18 +25,19 @@ const BillDocument = ({data}: any) => {
     <Document>
       <Page size="A4" style={styles.page}>
         <View style={styles.billSection}>
-          <Text>{bill.amount} split between {tennentList.length} people.</Text>
+          <Text>{formatPayment(bill.amount)} split between {tennentList.length} people.</Text>
         </View>
         <View style={styles.tennentSection}>
-          {tennentList.map((tennent: { name: string; bill: number; }, key: number) => {
+          {tennentList.map((tennent: { name: string; bill: number; startDate: Date; endDate: Date;}, key: number) => {
             return (
-              <Text key={key} style={(key % 2) ? styles.oddLine : styles.evenLine }>
-                {tennent.name}: ${tennent.bill}
-              </Text>
+              <View  key={key} style={(key % 2) ? styles.oddLine : styles.evenLine }>
+                <Text style={styles.tennentName}>{tennent.name}</Text>
+                <Text style={styles.tennentDates}>{new Intl.DateTimeFormat("en-US").format(tennent.startDate)}-{new Intl.DateTimeFormat("en-US").format(tennent.endDate)}</Text>
+                <Text style={styles.tennentBill}>{formatPayment(tennent.bill)}</Text>
+              </View>
             );
           })}
         </View>
-        
       </Page>
     </Document>
   )
